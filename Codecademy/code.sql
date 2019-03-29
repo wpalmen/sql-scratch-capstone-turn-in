3fae0acf7840e26bbd978f8f4edbbcfa112b7823
wpalmen <47602989+wpalmen@users.noreply.github.com>
2019-03-28 18:40:27 -0700

    Add files via upload

--- Codecademy/code.sql
+++ Codecademy/code.sql
@@ -29,3 +29,24 @@ on q.user_id=h.user_id
 left join purchase p
 on q.user_id=p.user_id
 LIMIT 10;
+
+WITH data_set AS
+(
+ SELECT
+   DISTINCT q.USER_ID,
+   h.user_id IS NOT NULL AS is_home_try_on,
+   h.number_of_pairs,
+   p.user_id IS NOT NULL AS is_purchase
+ FROM quiz q
+ LEFT JOIN home_try_on h
+ ON q.user_id = h.user_id
+ LEFT JOIN purchase p
+ ON q.user_id = p.user_id
+)
+ SELECT 
+ number_of_pairs,count(*) ,
+ SUM(is_home_try_on),
+ SUM(is_purchase),
+ 1.0 * SUM(is_purchase)/SUM(is_home_try_on) as percentage
+ FROM data_set
+ GROUP BY number_of_pairs;
